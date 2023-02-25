1331ae3b03fa29fac2eaf6b86bf60b336434f570
Luan Pontes <luanpontes2@gmail.com>
2023-02-25 15:18:52 -0300

    solution  poetry-club-door-policy

--- JavaScript/poetry-club-door-policy/door-policy.js
+++ JavaScript/poetry-club-door-policy/door-policy.js
@@ -27,7 +27,7 @@
  * @returns {string}
  */
 export function frontDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
+  return line[0].toUpperCase();
 }
 
 /**
@@ -38,7 +38,9 @@ export function frontDoorResponse(line) {
  * @returns {string} the front door password
  */
 export function frontDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
+  let first_word = word.split(' ')[0];
+
+  return first_word.charAt(0).toUpperCase() + first_word.slice(1).toLowerCase();
 }
 
 /**
@@ -47,9 +49,9 @@ export function frontDoorPassword(word) {
  *
  * @param {string} line
  * @returns {string}
- */
+ */ 
 export function backDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
+  return line.trim().slice(-1).toLowerCase();
 }
 
 /**
@@ -60,5 +62,5 @@ export function backDoorResponse(line) {
  * @returns {string} the back door password
  */
 export function backDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
+  return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase() + ', please';
 }
