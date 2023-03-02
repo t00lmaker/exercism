15e57bc3283cd43787c30d9b06d85dd750ae19f9
Luan Pontes <luanpontes2@gmail.com>
2023-03-01 21:54:30 -0300

    solution isbn-verifier

--- JavaScript/isbn-verifier/isbn-verifier.js
+++ JavaScript/isbn-verifier/isbn-verifier.js
@@ -3,6 +3,19 @@
 // convenience to get you started writing code faster.
 //
 
-export const isValid = () => {
-  throw new Error('Remove this statement and implement this function');
+export const isValid = (isbn) => {
+  const regexISBN = /^[0-9](-)?[0-9]{3}(-)?[0-9]{5}(-)?[0-9Xx]$/
+  if(regexISBN.test(isbn)){   
+    var acc = 0, num = 0;
+    isbn
+      .replace(/-/g,'')
+      .split('')
+      .forEach((c, i)=> { 
+        num = c === 'X' ? 10 : parseInt(c);
+        acc += num * (10-i) 
+      });
+    return acc % 11 === 0;
+  }
+  
+  return false;
 };
